47ec08cd0dca8f17e766588c23b9989c5b59b43a
roboto441 <roboto441@gmail.com>
2015-07-22 19:59:59 +0200

    Exemple

---
+++ exemple.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    /* How to use */
+    /*This are the default values  */
+    
+    simpleCarousel({
+        container: '.slider',       /* Container */
+        animationType 'slide',      /* Animation Type, can also be set to fade */
+        animation: 'easeInOutCirc', /* If you want to use easing functions, import the library and set the one you want */
+        duration: 400,              /* Duration of the animation */
+        timer: 3000,                /* Time between 2 automatic slides, if you don't want automatic animations set it to 0 */
+        width: '100%',              /* Width */
+        customBtn: false,           /* Choose if you want to style the buttons yourself */
+        dots: false,                /* If you want dots set it to true */
+        customDots: false           /* Choose if you want to style the dots yourself */
+    });
+});
